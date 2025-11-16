fe0e4a4a3c93cebe33e375f7d5df53c171b65141
Jaime <jaimeguantay28@gmail.com>
2025-11-16 02:15:07 -0300

    Initial commit

---
+++ src/main/java/jaime/oGProtocolMinecraft/OGProtocolMinecraft.java
@@ -0,0 +1,17 @@
+package jaime.oGProtocolMinecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OGProtocolMinecraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
